91d601f93747e0b186f56fab8c80e1fd670879e3
ryancui- <cuizhaowei92@gmail.com>
2018-01-18 22:35:39 +0800

    refactor: 团详情界面

--- src/components/OrdersPage.vue
+++ src/components/OrdersPage.vue
@@ -1,40 +1,46 @@
 <template>
   <div>
     <div class="group-overview" @click="showActionSheet()">
-      <div class="group-line">
-        <f-avatar :src="group.composer.avatar_url" :size="2"></f-avatar>
-        <span style="flex: 1;margin-left: 0.5rem;">{{group.composer.username}}</span>
-        <span style="color: red;width: 100px;font-size: 1.2rem;text-align: right;">￥{{group.totalPrice}}</span>
+      <div class="group-deadline">
+        截止时间为{{group.due_time}}
       </div>
-      <div class="group-line">
-        <span>{{group.name}}</span>
-      </div>
-      <div class="group-line" style="justify-content: space-between;">
-        <span>截止时间：{{group.due_time}}</span>
-        <span :style="getGroupStatusStyleObj(group.status)">{{formatGroupStatus(group.status)}}</span>
+      <div class="group">
+        <div class="group-avatar">
+          <f-avatar :src="group.composer.avatar_url" :size="40" :unit="'px'"></f-avatar>
+          <span class="group-composer">{{group.composer.username}}</span>
+        </div>
+        <div class="group-main">
+          <span class="group-name">{{group.name}}</span>
+          <div v-if="formatDueTime(group.due_time) > 0 && group.status === 1" class="group-due">
+            离截止时间还有<span style="color: red;">{{formatDueTime(group.due_time)}}</span>分钟
+          </div>
+          <div v-if="formatDueTime(group.due_time) <= 0 && group.status === 1" class="group-due">
+            已经截止啦~
+          </div>
+        </div>
+        <div class="group-status"
+             :style="getGroupStatusStyleObj(group.status)">
+          <span>{{formatGroupStatus(group.status) + '！'}}</span>
+        </div>
       </div>
     </div>
 
+    <sub-header class="order-sub-header" name="大家吃了什么"></sub-header>
     <div v-if="orders.length === 0" class="none-tips">
       不存在的
     </div>
     <div class="orders-block">
-      <group v-for="order in orders" :key="order.user.id">
-        <div slot="title" class="order-user">
-          <f-avatar :src="order.user.avatar_url" :size="2"></f-avatar>
-          <span style="flex: 1;margin-left: 0.5rem;">{{order.user.username}}</span>
-          <span style="color: red;width: 100px;font-size: 1.2rem;text-align: right;">￥{{order.totalPrice}}</span>
+      <div class="user-order" v-for="order in orders" :key="order.user.id">
+        <div class="user-detail">
+          <f-avatar :src="order.user.avatar_url" :size="25" unit="px"></f-avatar>
+          <span class="user-name">{{order.user.username}}</span>
+          <span class="user-price">￥{{order.totalPrice}}</span>
         </div>
-        <cell v-for="row in order.rows"
-              :key="row.id"
-              :title="row.product_name"
-              @click.native="confirmDelete(row)">
-          <div>
-            <span>× {{row.quantity}} = </span>
-            <span>{{'￥' + row.total_price}}</span>
-          </div>
-        </cell>
-      </group>
+        <div class="order-detail" v-for="row in order.rows" :key="row.id">
+          <span>{{row.product_name}}</span>
+          <span>￥{{row.total_price}}</span>
+        </div>
+      </div>
     </div>
 
     <div class="ff-btn bottom-btn" @click="navigateToProducts()">
@@ -53,6 +59,7 @@
   import { mapState } from 'vuex';
   import { XHeader, Group, Cell, Actionsheet } from 'vux';
   import FAvatar from '@/components/common/FAvatar';
+  import SubHeader from '@/components/common/SubHeader';
   import groupService from '@/api/group';
   import orderService from '@/api/order';
   import groupStatusMixin from '@/mixins/group-status';
@@ -60,7 +67,7 @@
   export default {
     mixins: [groupStatusMixin],
     components: {
-      XHeader, Group, Cell, FAvatar, Actionsheet
+      XHeader, Group, Cell, FAvatar, Actionsheet, SubHeader
     },
     created() {
       this.refreshGroupData(this.$route.params.groupId);
@@ -82,13 +89,18 @@
       }
     },
     methods: {
+      /**
+       * 刷新订单团信息
+       */
       refreshGroupData(groupId) {
         groupService.getGroupDetail(groupId).then(({data}) => {
           this.group = data;
           this.initOrders(data.orders);
         });
       },
-      // 按人初始化订单
+      /**
+       * 按人初始化订单
+       */
       initOrders(orders) {
         const result = [];
         orders.forEach(order => {
@@ -112,7 +124,21 @@
         this.orders = result;
         this.group.totalPrice = orders.map(o => o.total_price).reduce((p, c) => p + c, 0);
       },
-      // 确认是否删除订单
+      /**
+       * 格式化过期时间
+       * @param dueTime
+       */
+      formatDueTime(dueTime) {
+        if (!dueTime) {
+          return 0;
+        }
+
+        const date = new Date(dueTime.replace(/-/g, '/'));
+        return Math.floor((date.getTime() - Date.now()) / 1000 / 60);
+      },
+      /**
+       * 确认是否删除订单
+       */
       confirmDelete(order) {
         if (order.user_id !== this.user.id || this.group.status !== 1) {
           return;
@@ -167,46 +193,136 @@
 <style scoped lang="scss">
   @import 'src/styles/custom.scss';
 
-  $preview-height: 110px;
-
   .group-overview {
-    height: $preview-height;
-    padding: 0.5rem 1rem;
-    box-shadow: #e2e2e2 0 0.5rem 0.5rem;
+    box-shadow: 0 1px 3px $shadow-color;
+  }
+
+  .group-deadline {
+    height: 28px;
+    color: #FF5253;
+    font-size: 13px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: $white-color;
+  }
+
+  .group {
+    height: 80px;
+    padding: 9px 15px;
     background-color: white;
-  }
-
-  .group-line {
     display: flex;
     align-items: center;
-    margin: 0.5rem 0;
+    box-sizing: border-box;
+
+    .group-avatar {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      .group-composer {
+        width: 50px;
+        word-wrap: break-word;
+        font-size: 11px;
+        color: $grey-color;
+        text-align: center;
+        line-height: 11px;
+        padding-top: 4px;
+      }
+    }
+
+    .group-main {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      margin-left: 15px;
+      .group-name {
+        font-size: 17px;
+        color: $black-color;
+      }
+      .group-due {
+        font-size: 13px;
+        color: $grey-color;
+      }
+    }
+
+    .group-status {
+      width: 70px;
+      font-size: 17px;
+      text-align: right;
+    }
   }
 
-  .order-user {
-    display: flex;
-    align-items: center;
-    margin-left: 1rem;
-    margin-right: 1rem;
-    margin-top: 20px;
+  // 订单详情
+  .order-sub-header {
+    margin: 10px 0;
   }
 
   .orders-block {
-    overflow-y: auto;
-    -webkit-overflow-scrolling: touch;
     position: absolute;
-    top: $preview-height + 30px;
+    top: 155px;
     left: 0;
     right: 0;
-    bottom: 0;
+    bottom: 50px;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+    .user-order {
+      border-radius: 6px;
+      margin: 0 auto;
+      width: 90vw;
+      padding: 5px;
+      font-size: 17px;
+      margin-bottom: 10px;
+
+      &:nth-of-type(3n+1) {
+        background-color: #F6490D;
+      }
+      &:nth-of-type(3n+2) {
+        background-color: #59C0BE;
+      }
+      &:nth-of-type(3n+3) {
+        background-color: #4A4A4A;
+      }
+
+      .user-detail {
+        background-color: transparent;
+        display: flex;
+        align-items: center;
+        margin: 9px 0;
+        color: $white-color;
+      }
+      .user-name {
+        margin-left: 5px;
+        flex-grow: 1;
+      }
+      .user-price {
+        padding-right: 15px;
+      }
+      .order-detail {
+        background-color: $white-color;
+        height: 47px;
+        padding: 12px 15px 12px 10px;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-radius: 8px;
+        margin-bottom: 5px;
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+    }
   }
 
   .none-tips {
     text-align: center;
     font-size: 1rem;
-    color: #aaa;
+    color: $grey-color;
     padding: 20px;
   }
 
+  // 我也点餐按钮
   .bottom-btn {
     position: absolute;
     bottom: 0;
