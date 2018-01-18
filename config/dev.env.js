f564ce5eacaa770649b96c40e8e4fe53fa8c5d1d
ryancui- <cuizhaowei92@gmail.com>
2018-01-18 20:05:34 +0800

    chore: 配置两套开发命令，可切换 localhost 与本地 IP

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,15 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
+var devLocation = process.env.DEV_LOCATION;
+var apiHost = 'localhost';
+if (devLocation === 'home') {
+  apiHost = '192.168.1.104';
+}
+
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://localhost:8360/fastfood/api"',
+  API_HOST: apiHost,
+  API_ROOT: '"http://' + apiHost + ':8360/fastfood/api"',
   ASSET_ROOT: '"/"'
 })
