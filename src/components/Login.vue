c7957072047c7e9922305474f45f1a4a6153c70e
ryancui- <cuizhaowei92@gmail.com>
2018-01-19 10:09:35 +0800

    refactor: 登录页面样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="wrapper">
-    <div style="height: 50%;">
-      <group label-width="90px">
-        <x-input title="用户名"
-                 placeholder="输入用户名"
-                 v-model="params.username"
-                 :required="true"></x-input>
-        <x-input type="password"
-                 placeholder="输入密码"
-                 title="密码"
-                 v-model="params.password"
-                 :required="true"></x-input>
-      </group>
-      <div class="btn">
-        <x-button type="primary" @click.native="login()">登录</x-button>
+  <div>
+    <div class="logo">
+      <img :src="`${pathname}static/img/logo.png`" alt="Logo">
+    </div>
+
+    <div class="login-form">
+      <div class="form-row">
+        <span class="form-label">用户名</span>
+        <input type="text" v-model="params.username">
       </div>
+      <div class="form-row">
+        <span class="form-label">密码</span>
+        <input type="password" v-model="params.password">
+      </div>
+    </div>
+
+    <div class="ff-btn login-btn" @click="login()">
+      <span>登录</span>
     </div>
   </div>
 </template>
@@ -59,16 +61,46 @@
   };
 </script>
 
-<style scoped>
-  .wrapper {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+<style scoped lang="scss">
+  @import 'src/styles/custom.scss';
+
+  .logo {
+    text-align: center;
+    margin-top: 57px;
+    img {
+      height: 70px;
+    }
   }
 
-  .btn {
-    margin-top: 1rem;
-    padding: 0 1rem;
+  .login-form {
+    width: 75vw;
+    margin: 0 auto;
+    margin-top: 100px;
+    .form-row {
+      padding: 3px 10px;
+      border-bottom: 1px $theme-color solid;
+      display: flex;
+      align-items: center;
+      font-size: 17px;
+      margin: 30px 0;
+      .form-label {
+        color: $theme-color;
+        width: 100px;
+      }
+      input {
+        flex-grow: 1;
+        outline: 0;
+        border: 0;
+        background-color: transparent;
+        font-size: 17px;
+      }
+    }
+  }
+
+  .login-btn {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
   }
 </style>
