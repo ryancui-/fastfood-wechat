744cb4bd912fd893f209fa398e32c1bc76f08b9a
ryancui- <cuizhaowei92@gmail.com>
2018-01-25 09:28:30 +0800

    feat: 显示中奖弹出框

--- src/components/RandomPage.vue
+++ src/components/RandomPage.vue
@@ -8,8 +8,28 @@
       <img v-if="!running" class="stick-up" :src="`${pathname}static/img/random/stick_up.png`" alt="">
       <img v-if="running" class="stick-down" :src="`${pathname}static/img/random/stick_down.png`" alt="">
       <img class="machine" :src="`${pathname}static/img/random/machine.png`" alt="">
-      <img class="text" :src="`${pathname}static/img/random/text.png`" alt="">
       <img v-if="!running" class="arrow" :src="`${pathname}static/img/random/arrow.png`" alt="">
+    </div>
+    <div class="text-block"
+         :class="{'text-running': running}"
+         :style="{'background-image': `url(${pathname}static/img/random/text.png)`}">
+    </div>
+    <div class="envelope-block">
+      <img v-if="running" class="envelope" :class="{'envelope-running': running}"
+           :src="`${pathname}static/img/random/envelope.png`" alt="">
+    </div>
+
+    <div class="bg" :class="{'bg-running': running}" @click.self="stop()">
+      <div class="result" :class="{'result-running': running}">
+        <img :src="`${pathname}static/img/random/today.png`" alt="">
+        <span class="name">{{result}}</span>
+        <div class="btn" @click="confirmOrder()">
+          <span>我点啦!</span>
+        </div>
+        <div class="one-more-time" @click="oneMoreTime()">
+          <span>不好，再来一次</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,12 +46,16 @@
     data() {
       return {
         running: false,
+        result: '啦啦啦啦啦啦啦',
         groupId: null,
         startPoint: {x: null, y: null},
         pushed: false,
       }
     },
     methods: {
+      /**
+       * 点击摇杆准备
+       */
       onTouchStart(evt) {
         this.startPoint = {
           x: evt.touches[0].clientX,
@@ -45,20 +69,44 @@
           this.pushed = true;
         }
       },
+      /**
+       * 松开事件
+       */
       onTouchEnd(evt) {
         const x = evt.changedTouches[0].clientX, y = evt.changedTouches[0].clientY;
         if (this.pushed && Math.abs(x - this.startPoint.x) < 50 && y - this.startPoint.y > 100) {
+          this.pushed = false;
           this.running = true;
-          setTimeout(() => {
-            this.running = false;
-          }, 2000);
         }
+      },
+      /**
+       * 再抽一次
+       */
+      oneMoreTime() {
+        this.running = false;
+        setTimeout(() => {
+          this.running = true
+        }, 200);
+      },
+      /**
+       * 取消抽奖
+       */
+      stop() {
+        this.running = false;
+      },
+      /**
+       * 下订单了
+       */
+      confirmOrder() {
+        console.log(this.result);
       }
     }
   }
 </script>
 
 <style scoped lang="scss">
+  @import 'src/styles/custom.scss';
+
   .logo-text {
     margin-top: 21vw;
     text-align: center;
@@ -75,12 +123,7 @@
       position: absolute;
       width: 76vw;
     }
-    .text {
-      position: absolute;
-      left: 7.5vw;
-      top: 13vw;
-      width: 59vw;
-    }
+
     .stick-up {
       position: absolute;
       left: 75vw;
@@ -98,6 +141,165 @@
       left: 76vw;
       width: 9vw;
       top: 35vw;
+      animation: 1s flink infinite;
+    }
+
+    @keyframes flink {
+      0% {
+        transform: translateY(-4vw);
+      }
+      50% {
+        transform: translateY(0);
+      }
+      100% {
+        transform: translateY(-4vw);
+      }
+    }
+  }
+
+  .text-block {
+    position: relative;
+    margin-left: 7vw;
+    height: 17vw;
+    margin-top: 28vw;
+    overflow: hidden;
+    background-position: 7vw -4vw;
+    background-repeat: repeat-y;
+    background-size: 59vw;
+  }
+
+  .text-running {
+    transition: background-position 3s ease-out;
+    background-position: 7vw calc(-4vw - 15 * 24vw);
+  }
+
+  .envelope-block {
+    position: relative;
+    margin-left: 7vw;
+    height: 40vh;
+    margin-top: 13vw;
+    overflow: hidden;
+    .envelope {
+      position: absolute;
+      left: 20vw;
+      width: 33.5vw;
+      transform: translateY(-100%);
+    }
+    .envelope-running {
+      animation: 1.5s 1.5s envelope-rolling linear forwards, 0.1s 3s envelope-open linear forwards;
+    }
+
+    @keyframes envelope-rolling {
+      0% {
+        transform: translateY(-100%);
+      }
+      100% {
+        transform: translateY(100%);
+      }
+    }
+
+    @keyframes envelope-open {
+      from {
+        transform: translateY(100%);
+        opacity: 1;
+      }
+      to {
+        transform: scale(1.1);
+        opacity: 0;
+      }
+    }
+  }
+
+  .bg {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    background-color: rgba(0, 0, 0, 0.4);
+    justify-content: center;
+    align-items: center;
+    opacity: 0;
+
+    .result {
+      border-radius: 8px;
+      background-color: $white-color;
+      width: 75vw;
+      height: 85vw;
+      text-align: center;
+
+      img {
+        position: relative;
+        width: 91vw;
+        left: -8vw;
+        top: -12vw;
+      }
+
+      .name {
+        position: relative;
+        top: -52vw;
+        display: inline-block;
+        width: 25vw;
+        height: 16vw;
+        color: $white-color;
+        font-size: 4.5vw;
+        overflow: hidden;
+      }
+      .btn {
+        background-color: $theme-color;
+        height: 13vw;
+        width: 60vw;
+        padding: 10px 0;
+        font-size: 4.5vw;
+        margin: 0 auto;
+        border-radius: 8px;
+        position: relative;
+        top: -30vw;
+        box-sizing: border-box;
+        color: $white-color;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .one-more-time {
+        margin-top: 6px;
+        font-size: 4vw;
+        color: $grey-color;
+        position: relative;
+        top: -30vw;
+      }
+    }
+  }
+
+  .bg-running {
+    animation: 0.5s 3s bg-appear ease-out forwards;
+  }
+
+  .result-running {
+    animation: 0.5s 3s result-open ease-out forwards;
+  }
+
+  @keyframes bg-appear {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes result-open {
+    from {
+      transform: scale(0.2);
+      opacity: 0;
+    }
+    90% {
+      transform: scale(1.2);
+      opacity: 1;
+    }
+    to {
+      opacity: 1;
     }
   }
 </style>
