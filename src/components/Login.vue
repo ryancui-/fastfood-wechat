6a124b1b857f974e73f14bdfbf7078610e5b71b9
ryancui- <cuizhaowei92@gmail.com>
2018-01-25 10:44:55 +0800

    feat: 登录页样式调整

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,18 +6,20 @@
 
     <div class="login-form">
       <div class="form-row">
-        <span class="form-label">用户名</span>
+        <span class="form-label">账号</span>
         <input type="text" v-model="params.username">
       </div>
       <div class="form-row">
         <span class="form-label">密码</span>
         <input type="password" v-model="params.password">
       </div>
+      <div class="register-link" @click="toRegister()">没有账号？这里注册！</div>
+      <div class="ff-btn login-btn" @click="login()">
+        <span>登录</span>
+      </div>
     </div>
 
-    <div class="ff-btn login-btn" @click="login()">
-      <span>登录</span>
-    </div>
+
   </div>
 </template>
 
@@ -56,6 +58,9 @@
             this.$vux.toast.text(res.errmsg, 'middle');
           }
         });
+      },
+      toRegister() {
+        this.$vux.toast.text('程序猿沉迷奥德赛<br>无心编码', 'middle');
       }
     }
   };
@@ -66,23 +71,31 @@
 
   .logo {
     text-align: center;
-    margin-top: 57px;
+    margin-top: 80px;
     img {
       height: 70px;
     }
   }
 
   .login-form {
-    width: 75vw;
+    width: 77vw;
     margin: 0 auto;
-    margin-top: 100px;
+    margin-top: 25px;
+    height: 235px;
+    border-radius: 8px;
+    background-color: $white-color;
+    box-shadow: 0 1px 3px $shadow-color;
+    padding: 40px 26px 0 26px;
+    box-sizing: border-box;
     .form-row {
-      padding: 3px 10px;
+      padding: 14px 6px 5px 6px;
       border-bottom: 1px $theme-color solid;
       display: flex;
       align-items: center;
       font-size: 17px;
-      margin: 30px 0;
+      &:nth-child(2) {
+        margin: 15px 0 37px 0;
+      }
       .form-label {
         color: $theme-color;
         width: 100px;
@@ -93,14 +106,21 @@
         border: 0;
         background-color: transparent;
         font-size: 17px;
+        text-align: right;
       }
+    }
+
+    .register-link {
+      text-align: center;
+      font-size: 13px;
+      color: $grey-color;
+    }
+
+    .login-btn {
+      border-radius: 8px;
+      margin: 9px auto;
     }
   }
 
-  .login-btn {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  }
+
 </style>
